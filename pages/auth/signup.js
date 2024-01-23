085f3a8155508ce9e748e58abeedd49ecf285794
Willem <willemhumphrey@gmail.com>
2024-01-23 16:02:40 -0500

    install axios as dependency and setup signup page

--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -1,18 +1,56 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 export default () => {
-    return (
-        <form>
-            <h1>Sign up</h1>
-            <div style={{ margin: '5px' }}>
-                <div className="form-group">
-                    <label htmlFor="">Email Address</label>
-                    <input type="text" className="form-control" />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="">Password</label>
-                    <input type="password" className="form-control" />
-                </div>
-            </div>
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        '/api/users/signup',
+        {
+          email,
+          password,
+        },
+        {
+          baseURL: 'https://ticketing.dev',
+        }
+      );
+
+      console.log(response);
+    } catch (error) {
+      if (error) seterrors(error.response.data.errors);
+      console.log(error.response.data.errors);
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign up</h1>
+      <div style={{ margin: '5px' }}>
+        <div className='form-group'>
+          <label htmlFor=''>Email Address</label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className='form-control'
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor=''>Password</label>
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type='password'
+            className='form-control'
+          />
+        </div>
+      </div>
+      <button className='btn btn-primary'>Signup</button>
             <button className="btn btn-primary">Signup</button>
-        </form>
-    )
-}
+    </form>
+  );
+};
