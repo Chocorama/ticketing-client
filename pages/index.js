d9a59c141102d388aca57b368df461ce5ffb12cd
Willem <willemhumphrey@gmail.com>
2024-02-01 16:29:27 -0500

    create build client function
    This is to prevent writing -long url which we use to reroute the client SSR to the auth service- over and over again

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   //   axios.get('/api/users/currentuser');
@@ -6,24 +6,11 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  // server - getInitProps runs after refresh, link from page, or url submission
-  //code below is coming from client service so headers like cookies dont exist. however, we forward them from req (the browser)
-  if (typeof window === 'undefined') {
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentUser',
-      {
-        headers: req.headers,
-      }
-    );
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
 
-    return data;
-  } else {
-    // else on browser
-    // getInitProps runs on client after forwarded from another page on app
-    const { data } = await axios.get('/api/users/currentUser');
-    return data;
-  }
+  const { data } = await client.get('/api/users/currentuser');
+  return data;
 };
 
 export default LandingPage;
