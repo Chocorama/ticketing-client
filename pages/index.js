c2d01ba72d1adb21a0814cf0d7dc14726a60dd5e
Willem <willemhumphrey@gmail.com>
2024-01-31 20:11:18 -0500

    feat: Implement getInitialProps function
    Created the getInitialProps function to handle data fetching in a Next.js component. Depending on whether the function is executed on the server or client, it dynamically provides a different URL for fetching data. Additionally, the function now forwards all headers from the browser to ensure consistent behavior.

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,29 @@
-export default () => {
-    return <h1>Landing page</h1>
-}
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  //   axios.get('/api/users/currentuser');
+  console.log(currentUser);
+  return <h1>Landing page</h1>;
+};
+
+LandingPage.getInitialProps = async ({ req }) => {
+  // server - getInitProps runs after refresh, link from page, or url submission
+  //code below is coming from client service so headers like cookies dont exist. however, we forward them from req (the browser)
+  if (typeof window === 'undefined') {
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentUser',
+      {
+        headers: req.headers,
+      }
+    );
+
+    return data;
+  } else {
+    // else on browser
+    // getInitProps runs on client after forwarded from another page on app
+    const { data } = await axios.get('/api/users/currentUser');
+    return data;
+  }
+};
+
+export default LandingPage;
