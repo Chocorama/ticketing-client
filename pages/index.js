efa3995339a41fcf30015813c6923759dca2d975
Willem <willemhumphrey@gmail.com>
2024-02-01 18:16:02 -0500

    change text to signin/singedout

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  //   axios.get('/api/users/currentuser');
-  console.log(currentUser);
-  return <h1>Landing page</h1>;
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>you are not signed in</h1>
+  );
 };
 
 LandingPage.getInitialProps = async (context) => {
