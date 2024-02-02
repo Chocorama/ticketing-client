3c9b437ab03e77d1cfa2712e1c162ebd94649344
Willem <willemhumphrey@gmail.com>
2024-02-02 10:37:13 -0500

    Use _App wrapper for retrieving and distributing currentUser props
    We initially used getInitialProps for the landing page. However to fetch currentUser props for the Header, Next only allows us to call getInitialProps one time.
    
    Here, we called it inside the _App's getInitialProps and forwarded on the headers

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,33 @@
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-export default ({ Component, pageProps }) => {
-    return <Component {...pageProps} />
-}
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <h1>Header! {currentUser.email}</h1>
+      <Component {...pageProps} />
+    </div>
+  );
+};
+
+// distribute currentUser to Header and LandingPage
+// We need two things from appContext -
+// 1: forward headers from the req object in build client since they contain {host: 'ticketing.dev'} and cookies
+// 2: call getInitialProps on Landing page
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentUser');
+  let pageProps = {};
+  if (appContext?.Component?.getInitialProps) {
+    // call getInitialProps on LandingPage, because by default Next only calls one
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  //pageProps and data === same currentUser obj
+  return {
+    pageProps,
+    ...data,
+  };
+};
+
+export default AppComponent;
